44e0673202d070ff3d75c33579fe496fa6b1fc7c
Afonnos <rebekajansongomes@gmail.com>
2025-12-03 22:23:21 -0300

    Implement factorial function in fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+
+int fatorial(int n) {
+    if (n < 0) {
+        return 1;
+    }
+
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    int resultado = 1;
+    for (int i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+    
+    return resultado;
+}
